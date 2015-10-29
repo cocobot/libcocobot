e93c7483a26c734fd503d0525a4da05e59ecc253
Damien Espitallier <damien.espitallier@gmail.com>
2015-10-29 20:02:08 +0100

    add cocobot_ prefix on trajectory module fix pause/resume functions definitions add time unit on comments

--- include/cocobot/trajectory.h
+++ include/cocobot/trajectory.h
@@ -1,89 +1,89 @@
-#ifndef TRAJECTORY_H
-#define TRAJECTORY_H
+#ifndef cocobot_trajectory_H
+#define cocobot_trajectory_H
 
-#define TRAJECTORY_UNLIMITED_TIME (-1f)
+#define COCOBOT_TRAJECTORY_UNLIMITED_TIME (-1f)
 
 /* Type for storage of order unique handle */
-typedef uint32_t trajectory_handle_t;
+typedef uint32_t cocobot_trajectory_handle_t;
 
-/* Return values of trajectory_wait and trajectory_wait_until */
+/* Return values of cocobot_trajectory_wait and cocobot_trajectory_wait_until */
 typedef enum
 {
-  TRAJECTORY_SUCCESS,             /* Orders have been proccessed without error */
-  TRAJECTORY_STOPPED_BEFORE_END,  /* trajectory_stop has been called before the end of the order list */
-  TRAJECTORY_TIMEOUT_BEFORE_END,  /* One (or more) order has timouted before reaching the target */
-} trajectory_result_t;
+  COCOBOT_TRAJECTORY_SUCCESS,             /* Orders have been proccessed without error */
+  COCOBOT_TRAJECTORY_STOPPED_BEFORE_END,  /* cocobot_trajectory_stop has been called before the end of the order list */
+  COCOBOT_TRAJECTORY_TIMEOUT_BEFORE_END,  /* One (or more) order has timouted before reaching the target */
+} cocobot_trajectory_result_t;
 
 
 /* Initialization of the trajectory module. Need to be called before any other action */
-void trajectory_init(void);
+void cocobot_trajectory_init(void);
 
 
 /* Goto d order. 
  * Arguments:
  *  - distance: The target distance in mm
- *  - time: Maximum allowed time for the order (or TRAJECTORY_UNLIMITED_TIME for no timeout)
+ *  - time: Maximum allowed time in s for the order (or COCOBOT_TRAJECTORY_UNLIMITED_TIME for no timeout)
  *  Return:
  *    An unique id for order identification
  */
-trajectory_handle_t trajectory_goto_d(float distance, float time);
+cocobot_trajectory_handle_t cocobot_trajectory_goto_d(float distance, float time);
 
 /* Goto a order. 
  * Arguments:
  *  - angle: The target angle in deg
- *  - time: Maximum allowed time for the order (or TRAJECTORY_UNLIMITED_TIME for no timeout)
+ *  - time: Maximum allowed time in s for the order (or COCOBOT_TRAJECTORY_UNLIMITED_TIME for no timeout)
  *  Return:
  *    An unique id for order identification
  */
-trajectory_handle_t trajectory_goto_a(float angle, float time);
+cocobot_trajectory_handle_t cocobot_trajectory_goto_a(float angle, float time);
 
 /* Goto xy order. 
  * Arguments:
  *  - x: The target x coordonate in mm
  *  - y: The target y coordonate in mm
- *  - time: Maximum allowed time for the order (or TRAJECTORY_UNLIMITED_TIME for no timeout)
+ *  - time: Maximum allowed time in s for the order (or COCOBOT_TRAJECTORY_UNLIMITED_TIME for no timeout)
  *  Return:
  *    An unique id for order identification
  */
-trajectory_handle_t trajectory_goto_xy(float x, float y, float time);
+cocobot_trajectory_handle_t cocobot_trajectory_goto_xy(float x, float y, float time);
 
 /* Goto xy order but in backward. 
  * Arguments:
  *  - x: The target x coordonate in mm
  *  - y: The target y coordonate in mm
- *  - time: Maximum allowed time for the order (or TRAJECTORY_UNLIMITED_TIME for no timeout)
+ *  - time: Maximum allowed time in s for the order (or COCOBOT_TRAJECTORY_UNLIMITED_TIME for no timeout)
  *  Return:
  *    An unique id for order identification
  */
-trajectory_handle_t trajectory_goto_xy_backward(float x, float y, float time);
+cocobot_trajectory_handle_t cocobot_trajectory_goto_xy_backward(float x, float y, float time);
 
 
-/* Block the task until all the orders has been processed or trajectory_stop has been called 
+/* Block the task until all the orders has been processed or cocobot_trajectory_stop has been called 
  * Return:
- *  trajectory_result_t value
+ *  cocobot_trajectory_result_t value
  */
-trajectory_result_t trajectory_wait(void);
+cocobot_trajectory_result_t cocobot_trajectory_wait(void);
 
-/* Block the task until the order in argument has been processed or trajectory_stop has been called 
+/* Block the task until the order in argument has been processed or cocobot_trajectory_stop has been called 
  * Argument:
  *  - order: an handle to the order
  * Return:
- *  trajectory_result_t value
+ *  cocobot_trajectory_result_t value
  */
-trajectory_result_t trajectory_wait(trajectory_handle_t order);
+cocobot_trajectory_result_t cocobot_trajectory_wait(cocobot_trajectory_handle_t order);
 
 
 /* Stop all orders
  */
-void trajectory_stop(void):
+void cocobot_trajectory_stop(void);
 
-/* Pause orders (can be resume by calling trajectory_resume). Robot will wait without moving
+/* Pause orders (can be resume by calling cocobot_trajectory_resume). Robot will wait without moving
  */
-void trajectory_pause(void):
+void cocobot_trajectory_pause(void);
 
 /* Resume paused orders
  */
-void trajectory_resume(void):
+void cocobot_trajectory_resume(void);
 
 
-#endif// TRAJECTORY_H
+#endif// cocobot_trajectory_H
