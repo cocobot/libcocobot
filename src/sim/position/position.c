50c2e3def4531e1ba2c48a3126b60da63af66f3c
Damien Espitallier <damien.espitallier@gmail.com>
2016-01-12 19:26:50 +0100

    fix sim ulationerror on my **slowÃ** computer

--- src/sim/position/position.c
+++ src/sim/position/position.c
@@ -5,15 +5,31 @@
 #include <platform.h>
 #include "generated/autoconf.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include "extApi.h"
 
 #define DEG2RAD(angle)  ((((float)angle) * M_PI) / ((float)180))
 
-int clientID=-1;
 
-int   left_motor_handle=-1,         right_motor_handle=-1;
-float left_motor_position=0,        right_motor_position=0;
-float left_motor_prev_position=0,   right_motor_prev_position=0;
+typedef struct 
+{
+  int initialized;
+  float left_motor_position;
+  float right_motor_position;
+} cocobot_from_vrep_t;
+
+typedef struct 
+{
+  int initialized;
+  float left_motor_speed;
+  float right_motor_speed;
+} cocobot_to_vrep_t;
+
+static cocobot_from_vrep_t buffer_from_vrep;
+static cocobot_to_vrep_t buffer_to_vrep;
+
+static pthread_mutex_t lock;
 
 float robot_x=0, robot_y=0;
 
@@ -25,22 +41,23 @@ static void cocobot_position_task(void * arg)
   //arg is always NULL. Prevent "variable unused" warning
   (void)arg;
 
+  cocobot_from_vrep_t buffer_from_vrep_sync;
   TickType_t xLastWakeTime;
   xLastWakeTime = xTaskGetTickCount();
   while(1)
   {
-    if (simxGetConnectionId(clientID)!=-1)
-    {
-      // update motors values
-      simxGetJointPosition(clientID, left_motor_handle, &left_motor_position, simx_opmode_buffer);
-      simxGetJointPosition(clientID, right_motor_handle, &right_motor_position, simx_opmode_buffer);
+    pthread_mutex_lock(&lock);
+    memcpy(&buffer_from_vrep_sync, &buffer_from_vrep, sizeof(cocobot_from_vrep_t));
+    pthread_mutex_unlock(&lock);
 
+    if(buffer_from_vrep_sync.initialized) 
+    {
       //compute new curvilinear distance
-      float new_distance = left_motor_position + right_motor_position;
+      float new_distance = buffer_from_vrep_sync.left_motor_position + buffer_from_vrep_sync.right_motor_position;
       float delta_distance = new_distance - robot_distance;
 
       //compute new angle value
-      float new_angle = left_motor_position - right_motor_position;
+      float new_angle = buffer_from_vrep_sync.left_motor_position - buffer_from_vrep_sync.right_motor_position;
       float delta_angle = new_angle - robot_angle;
 
       //compute X/Y coordonate
@@ -57,26 +74,29 @@ static void cocobot_position_task(void * arg)
 
       //run the asserv
       cocobot_asserv_compute();
+    }
 
-      //wait 10ms
-      vTaskDelayUntil( &xLastWakeTime, 10 / portTICK_PERIOD_MS);
-    }
-    else
-    {
-      printf("cocobot_position_task: Connection with VREP remote API server lost\n");
-      //wait 1s
-      vTaskDelayUntil( &xLastWakeTime, 1000 / portTICK_PERIOD_MS);
-    }
+    //wait 10ms
+    vTaskDelayUntil( &xLastWakeTime, 10 / portTICK_PERIOD_MS);
   }
 
 }
 
-
-void cocobot_position_init(unsigned int task_priority)
+static void * cocobot_position_vrep(void * args)
 {
+  (void)args;
+
   // Start Vrep server
-  int vrep_server_port=1234;
-  int ret=0;
+  int vrep_server_port = 1234;
+  int ret = 0;
+  int clientID = -1;
+  int left_motor_handle = -1;
+  int right_motor_handle = -1;
+
+  cocobot_to_vrep_t buffer_to_vrep_sync;
+  cocobot_from_vrep_t buffer_from_vrep_unsync;
+  buffer_from_vrep_unsync.initialized = 1;
+
   clientID = simxStart((simxChar*)"127.0.0.1", vrep_server_port, 1, 1, 2000, 5);
   if (clientID!=-1)
   {
@@ -87,28 +107,61 @@ void cocobot_position_init(unsigned int task_priority)
     {
       printf("Error %d during remote function call. Could not get object cocobotLeftMotor#\n", ret);
     }
-    else
-    {
-      simxGetJointPosition(clientID, left_motor_handle, &left_motor_position, simx_opmode_streaming);
-    }
 
     ret = simxGetObjectHandle(clientID, "cocobotRightMotor#", &right_motor_handle, simx_opmode_oneshot_wait);
     if (ret != 0)
     {
       printf("Error %d during remote function call. Could not get object cocobotRightMotor#\n", ret);
     }
-    else
-    {
-      simxGetJointPosition(clientID, right_motor_handle, &right_motor_position, simx_opmode_streaming);
-    }
 
-    // Start task
-    xTaskCreate(cocobot_position_task, "position", 200, NULL, task_priority, NULL);
+
+    while (simxGetConnectionId(clientID)!=-1)
+    {
+      simxGetJointPosition(clientID, left_motor_handle, &buffer_from_vrep_unsync.left_motor_position, simx_opmode_buffer);
+      simxGetJointPosition(clientID, right_motor_handle, &buffer_from_vrep_unsync.right_motor_position, simx_opmode_buffer);
+
+      pthread_mutex_lock(&lock);
+      memcpy(&buffer_from_vrep, &buffer_from_vrep_unsync, sizeof(cocobot_from_vrep_t));
+      memcpy(&buffer_to_vrep_sync, &buffer_to_vrep, sizeof(cocobot_to_vrep_t));
+      pthread_mutex_unlock(&lock);
+
+      if(buffer_to_vrep_sync.initialized)
+      {
+        simxSetJointTargetVelocity(clientID, left_motor_handle, buffer_to_vrep_sync.left_motor_speed, simx_opmode_oneshot);
+        simxSetJointTargetVelocity(clientID, right_motor_handle, buffer_to_vrep_sync.right_motor_speed, simx_opmode_oneshot);
+      }
+
+      extApi_sleepMs(5);
+    }
   }
   else
   {
     printf("cocobot_position_init: Failed connecting to VREP remote API server with port %d\n", vrep_server_port);
+    exit(EXIT_FAILURE);
   }
+}
+
+
+void cocobot_position_init(unsigned int task_priority)
+{
+  buffer_from_vrep.initialized = 0;
+  buffer_to_vrep.initialized = 0;
+
+  if (pthread_mutex_init(&lock, NULL) != 0)
+  {
+    perror("Unable to create mutex for V-REP");
+    exit(EXIT_FAILURE);
+  }
+
+  pthread_t vrep_thread;
+  if(pthread_create(&vrep_thread, NULL, cocobot_position_vrep, NULL) != 0)
+  {
+    perror("Unable to create thread for V-REP");
+    exit(EXIT_FAILURE);
+  }
+
+  // Start task
+  xTaskCreate(cocobot_position_task, "position", 200, NULL, task_priority, NULL); 
 }
 
 float cocobot_position_get_x(void)
@@ -143,25 +196,18 @@ float cocobot_position_get_speed_angle(void)
 
 void cocobot_position_set_motor_command(float left_motor_speed, float right_motor_speed)
 {
-  if (simxGetConnectionId(clientID)!=-1)
-  {
-    simxSetJointTargetVelocity(clientID, left_motor_handle, left_motor_speed, simx_opmode_oneshot);
-    simxSetJointTargetVelocity(clientID, right_motor_handle, right_motor_speed, simx_opmode_oneshot);
-  }
-  else
-  {
-      printf("cocobot_position_set_motor_command: No connection with VREP remote API server");
-  }
+   cocobot_to_vrep_t buffer_to_vrep_unsync;
+   
+   buffer_to_vrep_unsync.initialized = 1;
+   buffer_to_vrep_unsync.left_motor_speed = left_motor_speed;
+   buffer_to_vrep_unsync.right_motor_speed = right_motor_speed;
+
+   pthread_mutex_lock(&lock);
+   memcpy(&buffer_to_vrep, &buffer_to_vrep_unsync, sizeof(cocobot_to_vrep_t));
+   pthread_mutex_unlock(&lock);
 }
 
 void cocobot_position_set_speed_distance_angle(float linear_speed, float angular_velocity)
 {
-  if (simxGetConnectionId(clientID)!=-1)
-  {
-    cocobot_position_set_motor_command(linear_speed+angular_velocity, linear_speed-angular_velocity);
-  }
-  else
-  {
-      printf("cocobot_position_set_speed_distance_angle: No connection with VREP remote API server");
-  }
+  cocobot_position_set_motor_command(linear_speed+angular_velocity, linear_speed-angular_velocity);
 }
