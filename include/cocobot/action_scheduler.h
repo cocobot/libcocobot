366bbdfdfcde364edf75133bdd6b521217ca098a
David Bitonneau <david.bitonneau@gmail.com>
2016-02-27 17:54:33 +0100

    Action scheduler: first formula to evaluate actions
    Adding function to approximate time needed by the robot to reach the action's
    starting point

--- include/cocobot/action_scheduler.h
+++ include/cocobot/action_scheduler.h
@@ -8,8 +8,8 @@ typedef enum
 } cocobot_strategy_t;
 
 // Should never return 0 (reserved).
-// Should return a stricly positive value if execution was done correctly.
-// Should return a stricly negative value when action could not be fully
+// Should return a strictly positive value if execution was done correctly.
+// Should return a strictly negative value when action could not be fully
 // executed and should be done later.
 typedef int (*action_callback)(void);
 
@@ -24,15 +24,23 @@ void cocobot_action_scheduler_init(void);
  */
 void cocobot_action_scheduler_set_strategy(cocobot_strategy_t strat);
 
+/* Set the average linear speed of the robot during a match. This data is
+ * used to approximate the time needed to reach the action's starting point.
+ * By default the scheduler will consider that the robot needs no time to
+ * reach the action's starting point, which might bring unwanted results
+ * (e.g. action not completed before the end of the match).
+ */
+void cocobot_action_scheduler_set_average_linear_speed(float speed);
+
 /* Add a new action to the scheduler
  * Argument:
  *  - score:          points given when action succeed
  *  - x, y, a:        position (mm) and angle (deg), relatively to the table, to start the action
  *  - execution_time: time needed to guarantee the action's full execution
  *  - success_proba:  probability that the action succeed when doing it (between 0 and 1)
- *  - callback:       funtion to call when action should be executed
+ *  - callback:       function to call when action should be executed
  */
-void cocobot_action_scheduler_add_action(unsigned int score, float x, float y, float a, float execution_time, float risk, action_callback callback);
+void cocobot_action_scheduler_add_action(unsigned int score, float x, float y, float a, float execution_time, float success_proba, action_callback callback);
 
 /* Execute best remaining action based on game's state (selected strategy,
  * remaining time, distance, obstacles, ...)
