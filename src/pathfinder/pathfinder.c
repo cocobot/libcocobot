4e88425c68faed7961f9667bd27db1ef540ac7d8
vfaure <vincent.hr.faure@gmail.com>
2016-05-22 22:29:49 +0200

    parsing the table into a file

--- src/pathfinder/pathfinder.c
+++ src/pathfinder/pathfinder.c
@@ -1,5 +1,6 @@
 #include <cocobot.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "cocobot/pathfinder_internal.h"
 #include "cocobot/pathfinder_table.h"
 
@@ -127,6 +128,20 @@ char cocobot_pathfinder_execute_trajectory(int16_t starting_point_x, int16_t sta
     }
     
     cocobot_pathfinder_set_trajectory(&final_traj);
+
+    if(strcmp(getenv("DEBUG_TABLE"), "1") == 0)
+    {
+        FILE* f = fopen("debug_table.txt","w+");
+        for(int i=0; i<TABLE_LENGTH/GRID_SIZE; i++)
+        {
+            for(int j=0; j<TABLE_WIDTH/GRID_SIZE; j++)
+            {
+                fwrite(&g_table[i][j].nodeType, sizeof(uint16_t), 1, f);
+            }
+        }
+        fclose(f);
+    }
+    
     return TRAJECTORY_READY;
 }
 
